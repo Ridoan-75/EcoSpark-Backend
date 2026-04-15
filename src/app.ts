3af6d71df6b35d091aed4f10a1198bb218481066
Md Ridoan <ridoan437@gmail.com>
2026-04-15 19:50:23 +0600

    app.ts cors updated

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ app.use(
   cors({
     origin: [
       "https://ecospark-frontend-gules.vercel.app",
+      "http://localhost:3000"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
